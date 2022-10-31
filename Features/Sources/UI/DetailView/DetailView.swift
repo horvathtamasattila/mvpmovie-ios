5928c2bdb43539991cae47e136e6b98eb23ffefb
Tamas Horvath <tamas.horvath@makery.co>
2022-10-31 21:22:56 +0100

    fix

--- Features/Sources/UI/DetailView/DetailView.swift
+++ Features/Sources/UI/DetailView/DetailView.swift
@@ -8,44 +8,48 @@ struct DetailView: View {
     var body: some View {
 		ZStack {
 			Color.primaryBackground.edgesIgnoringSafeArea(.all)
-			VStack(alignment: .leading, spacing: 0) {
-				KFImage(media.backdrop_path)
-					.resizable()
-					.frame(maxWidth: UIScreen.main.bounds.width)
-					.aspectRatio(16/9, contentMode: .fit)
-					.padding(.bottom, 16)
-					.overlay(
-						VStack {
-							Spacer()
-							HStack {
+			ScrollView {
+				VStack(alignment: .leading, spacing: 0) {
+					KFImage(media.backdrop_path)
+						.resizable()
+						.frame(maxWidth: UIScreen.main.bounds.width)
+						.aspectRatio(16/9, contentMode: .fit)
+						.cornerRadius(radius: 16, corners: [.bottomLeft, .bottomRight])
+						.padding(.bottom, 16)
+						.overlay(
+							VStack {
 								Spacer()
-								RatingView(rating: media.vote_average)
-									.padding(4)
-									.background(Color.primaryBackground)
-									.cornerRadius(8)
-									.padding(.bottom, 24)
+								HStack {
+									Spacer()
+									RatingView(rating: media.vote_average)
+										.padding(4)
+										.background(Color.primaryBackground)
+										.cornerRadius(8)
+										.padding(.bottom, 24)
+										.padding(.trailing, 16)
+								}
 							}
+						)
+					Text(media.title)
+						.font(.title)
+						.padding(.horizontal, 16)
+						.padding(.bottom, 8)
+					HStack(spacing: 16) {
+						HStack(spacing: 4) {
+							Image(systemName: "bookmark")
+							Text("Save")
 						}
-					)
-				Text(media.title)
-					.font(.title)
+						HStack(spacing: 4) {
+							Image(systemName: "eye.slash")
+							Text("Hide")
+						}
+					}
 					.padding(.horizontal, 16)
-					.padding(.bottom, 8)
-				HStack(spacing: 16) {
-					HStack(spacing: 4) {
-						Image(systemName: "bookmark")
-						Text("Save")
-					}
-					HStack(spacing: 4) {
-						Image(systemName: "eye.slash")
-						Text("Hide")
-					}
+					.padding(.bottom, 24)
+					Text(media.overview)
+						.padding(.horizontal, 16)
+					Spacer()
 				}
-				.padding(.horizontal, 16)
-				.padding(.bottom, 24)
-				Text(media.overview)
-					.padding(.horizontal, 16)
-				Spacer()
 			}
 		}
 		.colorScheme(.dark)
