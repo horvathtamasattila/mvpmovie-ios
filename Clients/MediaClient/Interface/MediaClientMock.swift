8211bf4612d702103be18a71b9a7e6ab38f17c59
Tamas Horvath <tamas.horvath@makery.co>
2022-10-31 20:07:15 +0100

    chore: mock for search results

--- Clients/MediaClient/Interface/MediaClientMock.swift
+++ Clients/MediaClient/Interface/MediaClientMock.swift
@@ -4,7 +4,19 @@ import Foundation
 public extension MediaClient {
 	static var mock: Self {
 		return Self(
-			getMediaByName: { _ in Result.Publisher([]).eraseToAnyPublisher() }
+			getMediaByName: { _ in
+				let resource = "get_search_results_200"
+				let decoder = JSONDecoder()
+				do {
+					let url = Bundle.main.url(forResource: resource, withExtension: "json")
+					let data = try Data(contentsOf: url!)
+					let result = try decoder.decode(SearchResults.self, from: data)
+					return Result.Publisher(result.results).eraseToAnyPublisher()
+				} catch {
+					print(error)
+					return Result.Publisher([]).eraseToAnyPublisher()
+				}
+			}
 		)
 	}
 }
